c3fd9bd56e7297548486eb2357870c9206e9ca62
Kalpa Gunarathna <kalpa@sigscale.com>
2018-01-26 15:02:51 +0530

    export function

--- src/sccp_codec.erl
+++ src/sccp_codec.erl
@@ -29,7 +29,7 @@
 %% SCCP options codec funcion
 -export([party_address/1, nai/1, tt/1, numbering_plan/1, encoding_scheme/1,
 		routing_indicator/1, importance/1, refusal_cause/1, release_cause/1,
-		segmenting/1, return_cause/1, segmentation/1]).
+		segmenting/1, return_cause/1, segmentation/1, point_code/1]).
 %% functions to deal with SCCP optional part
 -export([optional_part/1, get_option/2, set_option/2]).
 
