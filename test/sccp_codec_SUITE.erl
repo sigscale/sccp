933a563d59c8f67bb09a923bb991e2fe44061f6c
Vance Shipley <vances@sigscale.org>
2023-03-05 19:45:07 +0000

    test cases with data from wireshark examples

--- test/sccp_codec_SUITE.erl
+++ test/sccp_codec_SUITE.erl
@@ -70,13 +70,13 @@ end_per_testcase(_TestCase, _Config) ->
 -spec sequences() -> Sequences :: [{SeqName :: atom(), Testcases :: [atom()]}].
 %% Group test cases into a test sequence.
 %%
-sequences() -> 
+sequences() ->
 	[].
 
 -spec all() -> TestCases :: [Case :: atom()].
 %% Returns a list of all test cases in this test suite.
 %%
-all() -> 
+all() ->
 	[party_address_gti_0, party_address_gti_1, party_address_gti_2, party_address_gti_3,
 		party_address_gti_4, nai, numbering_plan, encoding_scheme, importance,
 		refusal_cause, release_cause, return_cause, segmentation, point_code, ssn, bcd_even, bcd_odd,
@@ -85,7 +85,8 @@ all() ->
 		sccp_unitdata_service, sccp_expedited_data, sccp_expedited_ack, sccp_reset_request,
 		sccp_reset_confirmation, sccp_protocol_data_unit_error, sccp_inactivity_test,
 		sccp_extended_unitdata, sccp_extended_unitdata_service, sccp_long_unitdata,
-		sccp_long_unitdata_service].
+		sccp_long_unitdata_service, ws_cr, ws_cc, ws_df1, ws_ud, ws_rlsd, ws_rlc,
+		ws_xudt1, ws_xudt2, ws_xudts1, ws_xudts2, ws_xudts3].
 
 %%---------------------------------------------------------------------
 %%  Test cases
@@ -346,7 +347,7 @@ segmentation(_Config) ->
 	end,
 	C = rand:uniform(2) - 1,
 	RemSeg = rand:uniform(16) - 1,
-	S1 = #segmentation{first = First, class = C, remaning_seg = RemSeg},
+	S1 = #segmentation{first = First, class = C, remaining_seg = RemSeg},
 	S2 = sccp_codec:segmentation(S1),
 	true = is_binary(S2),
 	S2 = sccp_codec:segmentation(S1).
@@ -415,7 +416,9 @@ sccp_connection_req(_Config) ->
 	Rec = #sccp_connection_req{src_local_ref = SrcLocalRef, class = Class,
 			called_party = CalledParty, credit = Credit, calling_party = CallingParty,
 			data = Data, hop_counter = Hops, importance = Importance},
+erlang:display({?MODULE, ?LINE, Rec}),
 	Bin = sccp_codec:sccp(Rec),
+erlang:display({?MODULE, ?LINE, Bin}),
 	true = is_binary(Bin),
 	Rec = sccp_codec:sccp(Bin).
 
@@ -638,7 +641,7 @@ sccp_extended_unitdata(_Config) ->
 	CalledParty = gen_party_address(),
 	CallingParty = gen_party_address(),
 	Data = <<123:24>>,
-	Seg = #segmentation{first = true, class = 0, remaning_seg = 4},	
+	Seg = #segmentation{first = true, class = 0, remaining_seg = 4},
 	Importance = rand:uniform(5) - 1,
 	Rec = #sccp_extended_unitdata{class = Class, hop_counter = Hops,
 			called_party = CalledParty, calling_party = CallingParty, data = Data,
@@ -656,7 +659,7 @@ sccp_extended_unitdata_service(_Config) ->
 	CalledParty = gen_party_address(),
 	CallingParty = gen_party_address(),
 	Data = <<123:24>>,
-	Seg = #segmentation{first = true, class = 0, remaning_seg = 4},	
+	Seg = #segmentation{first = true, class = 0, remaining_seg = 4},
 	Importance = rand:uniform(5) - 1,
 	Rec = #sccp_extended_unitdata_service{return_cause = Cause, hop_counter = Hops,
 			called_party = CalledParty, calling_party = CallingParty, data = Data,
@@ -674,7 +677,7 @@ sccp_long_unitdata(_Config) ->
 	CalledParty = gen_party_address(),
 	CallingParty = gen_party_address(),
 	LongData = <<123:24>>,
-	Seg = #segmentation{first = true, class = 0, remaning_seg = 4},
+	Seg = #segmentation{first = true, class = 0, remaining_seg = 4},
 	Importance = rand:uniform(5) - 1,
 	Rec = #sccp_long_unitdata{class = Class, hop_counter = Hops,
 			called_party = CalledParty, calling_party = CallingParty, long_data = LongData,
@@ -692,7 +695,7 @@ sccp_long_unitdata_service(_Config) ->
 	CalledParty = gen_party_address(),
 	CallingParty = gen_party_address(),
 	LongData = <<123:24>>,
-	Seg = #segmentation{first = true, class = 0, remaning_seg = 4},
+	Seg = #segmentation{first = true, class = 0, remaining_seg = 4},
 	Importance = rand:uniform(5) - 1,
 	Rec = #sccp_long_unitdata_service{return_cause = Cause, hop_counter = Hops,
 			called_party = CalledParty, calling_party = CallingParty, long_data = LongData,
@@ -700,6 +703,341 @@ sccp_long_unitdata_service(_Config) ->
 	Bin = sccp_codec:sccp(Rec),
 	true = is_binary(Bin),
 	Rec = sccp_codec:sccp(Bin).
+
+ws_cr() ->
+	[{userdata, [{doc, "Wireshark sample Connection Request (Mobile Originating Call (AMR))"}]}].
+
+ws_cr(_Config) ->
+	Bin = <<1,2,7,32,2,2,7,5,195,142,0,32,0,4,5,195,142,0,16,0,15,71,
+			0,19,64,67,0,0,6,0,3,64,1,0,0,15,64,6,0,98,241,16,64,1,0,58,
+			64,8,0,98,241,16,64,1,129,149,0,16,64,17,16,6,39,0,3,64,16,
+			0,8,25,50,84,118,8,32,0,0,0,79,64,3,32,7,2,0,86,64,5,98,241,
+			16,0,1,0>>,
+	#sccp_connection_req{src_local_ref = 2098946,
+			class = 2,
+			called_party = #party_address{ri = true,
+					pc = 142, ssn = 32, _ = undefined},
+			calling_party = #party_address{ri = true,
+					pc = 142, ssn = 16, _ = undefined},
+			data = <<0,19,64,67,0,0,6,0,3,64,1,0,0,15,64,6,0,98,241,16,
+					64,1,0,58,64,8,0,98,241,16,64,1,129,149,0,16,64,17,16,
+					6,39,0,3,64,16,0,8,25,50,84,118,8,32,0,0,0,79,64,3,32,
+					7,2,0,86,64,5,98,241,16,0,1>>,
+			_ = undefined} = sccp_codec:sccp(Bin).
+
+ws_cc() ->
+	[{userdata, [{doc, "Wireshark sample Connection Confirm (UMTS_FP_MAC_RLC_RRC_NBAP)"}]}].
+
+ws_cc(_Config) ->
+	Bin = <<2,146,124,48,39,43,24,2,1,3,4,67,192,168,142,0>>,
+	#sccp_connection_confirm{dest_local_ref = 3177618,
+			src_local_ref = 1583911,
+			class = 2,
+			called_party = #party_address{ri = true,
+					pc = 10432, ssn = 142, _ = undefined},
+			_ = undefined} = sccp_codec:sccp(Bin).
+
+ws_df1() ->
+	[{userdata, [{doc, "Wireshark sample Data Form 1 (UMTS_FP_MAC_RLC_RRC_NBAP)"}]}].
+
+ws_df1(_Config) ->
+	Bin = <<6,146,124,48,0,1,12,0,17,64,8,0,0,1,0,57,64,1,16>>,
+	#sccp_data_form1{dest_local_ref = 3177618,
+			segmenting = false,
+			data = <<0,17,64,8,0,0,1,0,57,64,1,16>>} = sccp_codec:sccp(Bin).
+
+ws_ud() ->
+	[{userdata, [{doc, "Wireshark sample Unitdata (UMTS_FP_MAC_RLC_RRC_NBAP)"}]}].
+
+ws_ud(_Config) ->
+	Bin = <<9,128,48,2,4,2,66,142,41,0,14,64,37,64,0,3,0,3,
+			64,1,0,0,23,64,9,80,36,4,18,100,48,84,50,244,0,22,
+			64,1,0,0,0,0,96,64,5,4,244,17,0,2,2,66,142>>,
+	#sccp_unitdata{class = 128,
+			called_party = #party_address{ri = true,
+					ssn = 142, _ = undefined},
+			calling_party = #party_address{ri = true,
+					ssn = 142, _ = undefined},
+			data = <<0,14,64,37,64,0,3,0,3,64,1,0,0,23,64,9,
+					80,36,4,18,100,48,84,50,244,0,22,64,1,0,0,
+					0,0,96,64,5,4,244,17,0,2>>} = sccp_codec:sccp(Bin).
+
+ws_rlsd() ->
+	[{userdata, [{doc, "Wireshark sample Released (UMTS_FP_MAC_RLC_RRC_NBAP)"}]}].
+
+ws_rlsd(_Config) ->
+	Bin = <<4,146,124,48,39,43,24,3,0,0>>,
+	#sccp_released{dest_local_ref = 3177618,
+			src_local_ref = 1583911,
+			release_cause = sccp_user_originated,
+			_ = undefined} = sccp_codec:sccp(Bin).
+
+ws_rlc() ->
+	[{userdata, [{doc, "Wireshark sample Release Complete (Mobile Originating Call (AMR))"}]}].
+
+ws_rlc(_Config) ->
+	Bin = <<5,3,6,16,3,6,32>>,
+	#sccp_release_complete{dest_local_ref = 1050115,
+			src_local_ref = 2098691} = sccp_codec:sccp(Bin).
+
+ws_xudt1() ->
+	[{userdata, [{doc, "Wireshark sample Extended Unit Data Segmented (1 of 3) (from Bug 2059)"}]}].
+
+ws_xudt1(_Config) ->
+	Bin = <<17,129,4,4,14,16,255,10,18,6,0,18,4,121,82,68,51,34,2,
+			66,11,239,98,130,2,115,72,3,18,0,255,107,47,40,45,6,7,0,
+			17,134,5,1,1,1,160,34,97,32,128,2,7,128,161,14,6,12,42,134,
+			58,0,137,97,51,1,1,1,0,1,162,3,2,1,0,163,5,161,3,2,1,0,108,
+			128,161,36,2,1,1,2,1,23,48,28,160,26,48,11,128,1,18,129,1,
+			0,162,3,128,1,1,48,11,128,1,17,129,1,0,162,3,128,1,1,161,
+			18,2,1,2,2,1,19,48,10,131,0,190,6,130,1,1,132,1,2,161,21,
+			2,1,3,2,1,47,48,13,160,11,160,9,160,4,128,2,16,103,129,1,
+			127,161,67,2,1,4,2,1,32,48,59,193,7,2,144,80,68,4,0,32,226,
+			3,128,1,2,196,7,2,144,80,68,4,0,32,197,2,4,2,160,9,4,7,1,
+			16,80,68,4,0,32,164,10,48,8,2,1,5,161,3,2,1,1,158,7,131,19,
+			69,20,6,66,6,161,29,2,1,5,2,1,46,48,21,160,14,161,12,129,1,
+			0,130,4,0,0,0,0,131,1,0,161,3,128,16,4,194,1,0,0,0>>,
+	#sccp_extended_unitdata{class = 129,
+			hop_counter = 4,
+			called_party = #party_address{ri = false,
+					translation_type = 0,
+					numbering_plan = isdn_tele,
+					encoding_scheme = bcd_even,
+					nai = international,
+					gt = [9,7,2,5,4,4,3,3,2,2],
+					ssn = 6, _ = undefined},
+			calling_party = #party_address{ri = true,
+					ssn = 11, _ = undefined},
+			data = <<98,130,2,115,72,3,18,0,255,107,47,40,45,6,7,0,
+					17,134,5,1,1,1,160,34,97,32,128,2,7,128,161,14,6,12,42,134,
+					58,0,137,97,51,1,1,1,0,1,162,3,2,1,0,163,5,161,3,2,1,0,108,
+					128,161,36,2,1,1,2,1,23,48,28,160,26,48,11,128,1,18,129,1,
+					0,162,3,128,1,1,48,11,128,1,17,129,1,0,162,3,128,1,1,161,
+					18,2,1,2,2,1,19,48,10,131,0,190,6,130,1,1,132,1,2,161,21,
+					2,1,3,2,1,47,48,13,160,11,160,9,160,4,128,2,16,103,129,1,
+					127,161,67,2,1,4,2,1,32,48,59,193,7,2,144,80,68,4,0,32,226,
+					3,128,1,2,196,7,2,144,80,68,4,0,32,197,2,4,2,160,9,4,7,1,
+					16,80,68,4,0,32,164,10,48,8,2,1,5,161,3,2,1,1,158,7,131,19,
+					69,20,6,66,6,161,29,2,1,5,2,1,46,48,21,160,14,161,12,129,1,
+					0,130,4,0,0,0,0,131,1,0,161,3,128>>,
+			segmentation = #segmentation{first = true,
+					class = 1,
+					remaining_seg = 2,
+					seg_local_ref = 1},
+			_ = undefined} = sccp_codec:sccp(Bin).
+
+ws_xudt2() ->
+	[{userdata, [{doc, "Wireshark sample Extended Unit Data Segmented (2 of 3)  (from Bug 2059)"}]}].
+
+ws_xudt2(_Config) ->
+	Bin = <<17,129,4,4,14,16,255,10,18,6,0,18,4,121,82,68,51,34,2,
+			66,11,239,1,2,161,32,2,1,6,2,1,34,4,24,161,3,128,1,2,163,
+			17,160,3,128,1,3,164,10,4,8,1,1,16,80,68,4,0,32,161,67,2,
+			1,7,2,1,32,48,59,193,7,2,144,80,68,4,0,32,226,3,128,1,3,
+			196,7,2,144,80,68,4,0,32,197,2,4,2,160,9,4,7,1,16,80,68,
+			97,64,0,164,10,48,8,2,1,5,161,3,2,1,1,158,7,131,19,69,20,
+			6,66,6,161,29,2,1,8,2,1,46,48,21,160,14,161,12,129,1,0,
+			130,4,0,0,0,0,131,1,0,161,3,128,1,3,161,32,2,1,9,2,1,34,
+			4,24,161,3,128,1,3,163,17,160,3,128,1,3,164,10,4,8,1,1,
+			16,80,68,4,0,32,161,93,2,1,10,2,1,23,48,85,160,83,48,11,
+			128,1,249,129,1,0,162,3,128,1,3,48,11,128,1,13,129,1,0,
+			162,3,128,1,3,48,11,128,1,251,129,1,0,162,3,128,1,3,48,
+			16,128,1,14,129,1,0,162,3,128,1,3,190,3,129,1,20,16,4,
+			65,1,0,0,0>>,
+	#sccp_extended_unitdata{class = 129,
+			hop_counter = 4,
+			called_party = #party_address{ri = false,
+					translation_type = 0,
+					numbering_plan = isdn_tele,
+					encoding_scheme = bcd_even,
+					nai = international,
+					gt = [9,7,2,5,4,4,3,3,2,2],
+					ssn = 6, _ = undefined},
+			calling_party = #party_address{ri = true,
+					ssn = 11, _ = undefined},
+			data = <<1,2,161,32,2,1,6,2,1,34,4,24,161,3,128,1,2,163,
+					17,160,3,128,1,3,164,10,4,8,1,1,16,80,68,4,0,32,161,67,2,
+					1,7,2,1,32,48,59,193,7,2,144,80,68,4,0,32,226,3,128,1,3,
+					196,7,2,144,80,68,4,0,32,197,2,4,2,160,9,4,7,1,16,80,68,
+					97,64,0,164,10,48,8,2,1,5,161,3,2,1,1,158,7,131,19,69,20,
+					6,66,6,161,29,2,1,8,2,1,46,48,21,160,14,161,12,129,1,0,
+					130,4,0,0,0,0,131,1,0,161,3,128,1,3,161,32,2,1,9,2,1,34,
+					4,24,161,3,128,1,3,163,17,160,3,128,1,3,164,10,4,8,1,1,
+					16,80,68,4,0,32,161,93,2,1,10,2,1,23,48,85,160,83,48,11,
+					128,1,249,129,1,0,162,3,128,1,3,48,11,128,1,13,129,1,0,
+					162,3,128,1,3,48,11,128,1,251,129,1,0,162,3,128,1,3,48,
+					16,128,1,14,129,1,0,162,3,128,1,3,190,3,129,1,20>>,
+			segmentation = #segmentation{first = false,
+					class = 1,
+					remaining_seg = 1,
+					seg_local_ref = 1},
+			_ = undefined} = sccp_codec:sccp(Bin).
+
+ws_xudt3() ->
+	[{userdata, [{doc, "Wireshark sample Extended Unit Data Segmented (3 of 3) (from Bug 2059)"}]}].
+
+ws_xudt3(_Config) ->
+	Bin = <<17,129,4,4,14,16,169,10,18,6,0,18,4,121,82,68,51,34,
+			2,66,11,153,48,11,128,1,15,129,1,0,162,3,128,1,3,48,11,
+			128,1,250,129,1,1,162,3,128,1,3,161,93,2,1,11,2,1,23,48,
+			85,160,83,48,11,128,1,249,129,1,0,162,3,128,1,2,48,11,
+			128,1,13,129,1,0,162,3,128,1,2,48,11,128,1,251,129,1,0,
+			162,3,128,1,2,48,16,128,1,14,129,1,0,162,3,128,1,2,190,
+			3,129,1,20,48,11,128,1,15,129,1,0,162,3,128,1,2,48,11,
+			128,1,250,129,1,1,162,3,128,1,2,161,13,2,1,12,2,1,31,
+			225,5,161,3,128,1,2,161,13,2,1,13,2,1,31,225,5,161,3,
+			128,1,3,0,0,16,4,64,1,0,0,0>> ,
+	#sccp_extended_unitdata{class = 129,
+			hop_counter = 4,
+			called_party = #party_address{ri = false,
+					translation_type = 0,
+					numbering_plan = isdn_tele,
+					encoding_scheme = bcd_even,
+					nai = international,
+					gt = [9,7,2,5,4,4,3,3,2,2],
+					ssn = 6, _ = undefined},
+			calling_party = #party_address{ri = true,
+					ssn = 11, _ = undefined},
+			data = <<48,11,128,1,15,129,1,0,162,3,128,1,3,48,11,
+					128,1,250,129,1,1,162,3,128,1,3,161,93,2,1,11,2,1,23,48,
+					85,160,83,48,11,128,1,249,129,1,0,162,3,128,1,2,48,11,
+					128,1,13,129,1,0,162,3,128,1,2,48,11,128,1,251,129,1,0,
+					162,3,128,1,2,48,16,128,1,14,129,1,0,162,3,128,1,2,190,
+					3,129,1,20,48,11,128,1,15,129,1,0,162,3,128,1,2,48,11,
+					128,1,250,129,1,1,162,3,128,1,2,161,13,2,1,12,2,1,31,
+					225,5,161,3,128,1,2,161,13,2,1,13,2,1,31,225,5,161,3,
+					128,1,3,0,0>>,
+			segmentation = #segmentation{first = false,
+					class = 1,
+					remaining_seg = 0,
+					seg_local_ref = 1},
+			_ = undefined} = sccp_codec:sccp(Bin).
+
+ws_xudts1() ->
+	[{userdata, [{doc, "Wireshark sample Extended Unit Data Service Segmented (1 of 3) (from Bug 2059)"}]}].
+
+ws_xudts1(_Config) ->
+	Bin = <<18,0,3,4,6,10,249,2,66,11,4,67,134,3,0,239,
+			98,130,2,115,72,3,18,0,255,107,47,40,45,6,7,0,17,
+			134,5,1,1,1,160,34,97,32,128,2,7,128,161,14,6,12,
+			42,134,58,0,137,97,51,1,1,1,0,1,162,3,2,1,0,163,
+			5,161,3,2,1,0,108,128,161,36,2,1,1,2,1,23,48,28,
+			160,26,48,11,128,1,18,129,1,0,162,3,128,1,1,48,11,
+			128,1,17,129,1,0,162,3,128,1,1,161,18,2,1,2,2,1,19,
+			48,10,131,0,190,6,130,1,1,132,1,2,161,21,2,1,3,2,1,
+			47,48,13,160,11,160,9,160,4,128,2,16,103,129,1,127,
+			161,67,2,1,4,2,1,32,48,59,193,7,2,144,80,68,4,0,32,
+			226,3,128,1,2,196,7,2,144,80,68,4,0,32,197,2,4,2,
+			160,9,4,7,1,16,80,68,4,0,32,164,10,48,8,2,1,5,161,
+			3,2,1,1,158,7,131,19,69,20,6,66,6,161,29,2,1,5,2,1,
+			46,48,21,160,14,161,12,129,1,0,130,4,0,0,0,0,131,1,
+			0,161,3,128,16,4,194,2,0,0,0>>,
+	#sccp_extended_unitdata_service{return_cause = no_translation,
+			hop_counter = 3,
+			called_party = #party_address{ri = true,
+					ssn = 11, _ = undefined},
+			calling_party = #party_address{ri = true,
+					pc = 902,
+					ssn = 0, _ = undefined},
+			data = <<98,130,2,115,72,3,18,0,255,107,47,40,45,6,7,0,17,
+					134,5,1,1,1,160,34,97,32,128,2,7,128,161,14,6,12,
+					42,134,58,0,137,97,51,1,1,1,0,1,162,3,2,1,0,163,
+					5,161,3,2,1,0,108,128,161,36,2,1,1,2,1,23,48,28,
+					160,26,48,11,128,1,18,129,1,0,162,3,128,1,1,48,11,
+					128,1,17,129,1,0,162,3,128,1,1,161,18,2,1,2,2,1,19,
+					48,10,131,0,190,6,130,1,1,132,1,2,161,21,2,1,3,2,1,
+					47,48,13,160,11,160,9,160,4,128,2,16,103,129,1,127,
+					161,67,2,1,4,2,1,32,48,59,193,7,2,144,80,68,4,0,32,
+					226,3,128,1,2,196,7,2,144,80,68,4,0,32,197,2,4,2,
+					160,9,4,7,1,16,80,68,4,0,32,164,10,48,8,2,1,5,161,
+					3,2,1,1,158,7,131,19,69,20,6,66,6,161,29,2,1,5,2,1,
+					46,48,21,160,14,161,12,129,1,0,130,4,0,0,0,0,131,1,
+					0,161,3,128>>,
+			segmentation = #segmentation{first = true,
+					class = 1,
+					remaining_seg = 2,
+					seg_local_ref = 2},
+			_ = undefined} = sccp_codec:sccp(Bin).
+
+ws_xudts2() ->
+	[{userdata, [{doc, "Wireshark sample Extended Unit Data Service Segmented (2 of 3) (from Bug 2059)"}]}].
+
+ws_xudts2(_Config) ->
+	Bin = <<18,0,3,4,6,10,249,2,66,11,4,67,134,3,0,239,
+			1,2,161,32,2,1,6,2,1,34,4,24,161,3,128,
+			1,2,163,17,160,3,128,1,3,164,10,4,8,1,1,16,80,68,4,0,32,
+			161,67,2,1,7,2,1,32,48,59,193,7,2,144,80,68,4,0,32,226,
+			3,128,1,3,196,7,2,144,80,68,4,0,32,197,2,4,2,160,9,4,7,
+			1,16,80,68,97,64,0,164,10,48,8,2,1,5,161,3,2,1,1,158,7,
+			131,19,69,20,6,66,6,161,29,2,1,8,2,1,46,48,21,160,14,161,
+			12,129,1,0,130,4,0,0,0,0,131,1,0,161,3,128,1,3,161,32,2,
+			1,9,2,1,34,4,24,161,3,128,1,3,163,17,160,3,128,1,3,164,
+			10,4,8,1,1,16,80,68,4,0,32,161,93,2,1,10,2,1,23,48,85,
+			160,83,48,11,128,1,249,129,1,0,162,3,128,1,3,48,11,128,
+			1,13,129,1,0,162,3,128,1,3,48,11,128,1,251,129,1,0,162,
+			3,128,1,3,48,16,128,1,14,129,1,0,162,3,128,1,3,190,3,
+			129,1,20,16,4,65,2,0,0,0>>,
+	#sccp_extended_unitdata_service{return_cause = no_translation,
+			hop_counter = 3,
+			called_party = #party_address{ri = true,
+					ssn = 11, _ = undefined},
+			calling_party = #party_address{ri = true,
+					pc = 902,
+					ssn = 0, _ = undefined},
+			data = <<1,2,161,32,2,1,6,2,1,34,4,24,161,3,128,
+					1,2,163,17,160,3,128,1,3,164,10,4,8,1,1,16,80,68,4,0,32,
+					161,67,2,1,7,2,1,32,48,59,193,7,2,144,80,68,4,0,32,226,
+					3,128,1,3,196,7,2,144,80,68,4,0,32,197,2,4,2,160,9,4,7,
+					1,16,80,68,97,64,0,164,10,48,8,2,1,5,161,3,2,1,1,158,7,
+					131,19,69,20,6,66,6,161,29,2,1,8,2,1,46,48,21,160,14,161,
+					12,129,1,0,130,4,0,0,0,0,131,1,0,161,3,128,1,3,161,32,2,
+					1,9,2,1,34,4,24,161,3,128,1,3,163,17,160,3,128,1,3,164,
+					10,4,8,1,1,16,80,68,4,0,32,161,93,2,1,10,2,1,23,48,85,
+					160,83,48,11,128,1,249,129,1,0,162,3,128,1,3,48,11,128,
+					1,13,129,1,0,162,3,128,1,3,48,11,128,1,251,129,1,0,162,
+					3,128,1,3,48,16,128,1,14,129,1,0,162,3,128,1,3,190,3,
+					129,1,20>>,
+			segmentation = #segmentation{first = false,
+					class = 1,
+					remaining_seg = 1,
+					seg_local_ref = 2},
+			_ = undefined} = sccp_codec:sccp(Bin).
+
+ws_xudts3() ->
+	[{userdata, [{doc, "Wireshark sample Extended Unit Data Service Segmented (3 of 3) (from Bug 2059)"}]}].
+
+ws_xudts3(_Config) ->
+	Bin = <<18,0,3,4,6,10,163,2,66,11,4,67,134,3,0,153,
+			48,11,128,1,15,129,1,0,162,3,128,1,3,48,11,128,1,250,
+			129,1,1,162,3,128,1,3,161,93,2,1,11,2,1,23,48,85,160,
+			83,48,11,128,1,249,129,1,0,162,3,128,1,2,48,11,128,1,
+			13,129,1,0,162,3,128,1,2,48,11,128,1,251,129,1,0,162,
+			3,128,1,2,48,16,128,1,14,129,1,0,162,3,128,1,2,190,3,
+			129,1,20,48,11,128,1,15,129,1,0,162,3,128,1,2,48,11,
+			128,1,250,129,1,1,162,3,128,1,2,161,13,2,1,12,2,1,31,
+			225,5,161,3,128,1,2,161,13,2,1,13,2,1,31,225,5,161,3,
+			128,1,3,0,0,16,4,64,2,0,0,0>>,
+	#sccp_extended_unitdata_service{return_cause = no_translation,
+			hop_counter = 3,
+			called_party = #party_address{ri = true,
+					ssn = 11, _ = undefined},
+			calling_party = #party_address{ri = true,
+					pc = 902,
+					ssn = 0, _ = undefined},
+			data = <<48,11,128,1,15,129,1,0,162,3,128,1,3,48,11,128,1,250,
+					129,1,1,162,3,128,1,3,161,93,2,1,11,2,1,23,48,85,160,
+					83,48,11,128,1,249,129,1,0,162,3,128,1,2,48,11,128,1,
+					13,129,1,0,162,3,128,1,2,48,11,128,1,251,129,1,0,162,
+					3,128,1,2,48,16,128,1,14,129,1,0,162,3,128,1,2,190,3,
+					129,1,20,48,11,128,1,15,129,1,0,162,3,128,1,2,48,11,
+					128,1,250,129,1,1,162,3,128,1,2,161,13,2,1,12,2,1,31,
+					225,5,161,3,128,1,2,161,13,2,1,13,2,1,31,225,5,161,3,
+					128,1,3,0,0>>,
+			segmentation = #segmentation{first = false,
+					class = 1,
+					remaining_seg = 0,
+					seg_local_ref = 2},
+			_ = undefined} = sccp_codec:sccp(Bin).
 
 %%---------------------------------------------------------------------
 %%  Internal functions
